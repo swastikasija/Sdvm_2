23f463ec384c161058281ebb1f6d73873730a45b
swastik <swastikasija072@gmail.com>
2024-02-03 22:48:48 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,22 @@
+Shery.mouseFollower();
+Shery.textAnimate(".hero-content h1", {
+  style: 1,
+  y: 10,
+  delay: 0.1,
+  duration: 0.6,
+  ease: "cubic-bezier(0.23, 1, 0.320, 1)",
+  multiplier: 0.1,
+});
+Shery.textAnimate(".hero-content p", {
+  style: 2,
+  y: 10,
+  delay: 0.1,
+  duration: 0.6,
+  ease: "cubic-bezier(0.23, 1, 0.320, 1)",
+  multiplier: 0.1,
+});
+Shery.makeMagnet(".hero-content h1" /* Element to target.*/, {
+  //Parameters are optional.
+  ease: "cubic-bezier(0.23, 1, 0.320, 1)",
+  duration: 1,
+});
